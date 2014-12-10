1e5d30b9777f91902ca31d3096852073e699a452
Hiroaki Nakamura <hnakamur@gmail.com>
2014-12-10 14:32:31 +0900

    Update Vagrant to version 1.7.0

--- Casks/vagrant.rb
+++ Casks/vagrant.rb
@@ -1,8 +1,8 @@
 class Vagrant < Cask
-  version '1.6.5'
-  sha256 'a94a16b9ed5f63460f64110738067aea029238f8d826c8dd90c5c34615a5be1e'
+  version '1.7.0'
+  sha256 '245c66355d70a172fa8551ca7788dd4b4074f4b7104b898abbd9ec3a832d9b28'
 
-  url 'https://dl.bintray.com/mitchellh/vagrant/vagrant_1.6.5.dmg'
+  url 'https://dl.bintray.com/mitchellh/vagrant/vagrant_1.7.0.dmg'
   homepage 'http://www.vagrantup.com'
 
   install 'Vagrant.pkg'
